646a0c9f7dfddd45e60f1cdab016eb214114d01d
Darya Filatova <daria_filatova@interia.pl>
2024-12-10 22:51:44 +0100

    new version

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = create_app()
 
 if __name__ == '__main__':
     # Получить порт из переменных окружения или использовать 5000 по умолчанию
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000)) #9000
     
     # Прослушивать все интерфейсы (0.0.0.0) для Azure
     app.run(host='0.0.0.0', port=port, debug=True)
